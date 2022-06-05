d6e626d7a42b03a6532a98daa1c946c09c6d275c
yly <2337352929@qq.com>
2022-06-05 17:03:13 +0800

    IoC 之深入分析 Aware 接口

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -537,6 +537,16 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 	}
 
 	/**
+	 * AbstractAutowireCapableBeanFactory 的 #doCreateBean(final String beanName, final RootBeanDefinition mbd, final Object[] args) 方法，主要干三件事情：
+	 *
+	 * 实例化 bean 对象：#createBeanInstance(String beanName, RootBeanDefinition mbd, Object[] args) 方法。
+	 * 属性注入：#populateBean(String beanName, RootBeanDefinition mbd, BeanWrapper bw) 方法。
+	 * 初始化 bean 对象：#initializeBean(final String beanName, final Object bean, RootBeanDefinition mbd) 方法。
+	 * 而初始化 bean 对象时，也是干了三件事情：
+	 * 激活 Aware 方法
+	 * 后置处理器的应用
+	 * 激活自定义的 init 方法
+	 *
 	 * Actually create the specified bean. Pre-creation processing has already happened
 	 * at this point, e.g. checking {@code postProcessBeforeInstantiation} callbacks.
 	 * <p>Differentiates between default bean instantiation, use of a
@@ -1851,17 +1861,26 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		return wrappedBean;
 	}
 
+	/**
+	 * 首先，判断 bean 实例是否属于 Aware 接口的范畴，如果是的话，则调用实例的 setXxx() 方法给实例设置 xxx 属性值，在 #invokeAwareMethods(...) 方法，
+	 * 主要是设置 beanName，beanClassLoader、BeanFactory 中三个属性值。
+	 * @param beanName
+	 * @param bean
+	 */
 	private void invokeAwareMethods(String beanName, Object bean) {
 		if (bean instanceof Aware) {
+			// BeanNameAware
 			if (bean instanceof BeanNameAware) {
 				((BeanNameAware) bean).setBeanName(beanName);
 			}
+			// BeanClassLoaderAware
 			if (bean instanceof BeanClassLoaderAware) {
 				ClassLoader bcl = getBeanClassLoader();
 				if (bcl != null) {
 					((BeanClassLoaderAware) bean).setBeanClassLoader(bcl);
 				}
 			}
+			// BeanFactoryAware
 			if (bean instanceof BeanFactoryAware) {
 				((BeanFactoryAware) bean).setBeanFactory(AbstractAutowireCapableBeanFactory.this);
 			}
